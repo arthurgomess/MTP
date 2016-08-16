8479e307a5f8a1c1482bc16bb9d0293f704da196
Arthur Gomes <arthurgomes@gmail.com>
2016-08-16 10:21:04 -0300

    final de aula

---
+++ aula20160816/msg.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+        int i;         
+        if (argc == 1)
+               printf("bom dia mundo\n");
+
+        else
+             if(argc == 2)
+                      printf("Boa dia %s!\n", argv[1]);
+             else
+                   for(i=0; i<atoi(argv[1]); i++)
+                          printf("Boa dia %s!\n", argv[2]);
+             
+
+        return 0;
+}
