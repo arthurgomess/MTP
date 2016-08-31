800e0e632664f87a6a7f899f6bfec5b947a88228
arthurgomesba@gmail.com <arthurgomesba@gmail.com>
2016-08-30 21:19:43 -0300

    arquivo estava errado

--- aula20160830/cpx2.c
+++ aula20160830/cpx2.c
@@ -4,23 +4,19 @@
 int main()
 {
 
-    double complex Z1, Z2, soma;
-    double R1_z,I1_z,R2_z,I2_z;
+    double complex Z1;
+    double R1_z,I1_z;
     double mod;
 
-    printf("Real e imaginario de 1: \n");
+    printf("Real e imaginario : \n");
     scanf("%lf", &R1_z);
     scanf("%lf", &I1_z);
-    printf("Real e imaginario de 2: \n");
-    scanf("%lf", &R2_z);
-    scanf("%lf", &I2_z);
 
     Z1=R1_z+I1_z*I;
-    Z2=R2_z+I2_z*I;
 
-    soma= Z1+Z2;
+    mod= cabs(Z1);
 
-    printf("A soma e' %lf + &lf*I \n", creal(soma), cimg(soma));
+    printf("O produto desse numero complexo pelo seu conjugado e'= %f", mod);
     printf("\n\n");
     return 0;
 }
